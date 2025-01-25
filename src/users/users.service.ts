102197066a3b1dd612afa856385c17af0e31a101
bonfacemasira <bonfacemasira@gmail.com>
2025-01-25 16:19:31 +0300

    implement user crud on the sercies  and controller

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,50 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+  ) {}
+
+  createUser(createUserDto: CreateUserDto): Promise<User> {
+    const user: User = new User();
+    user.name = createUserDto.name;
+    user.age = createUserDto.age;
+    user.email = createUserDto.email;
+    user.username = createUserDto.username;
+    user.password = createUserDto.password;
+    user.gender = createUserDto.gender;
+    return this.userRepository.save(user);
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async findAllUsers(): Promise<User[]> {
+    return this.userRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOneUser(id: number): Promise<User> {
+    const user = await this.userRepository.findOneBy({ id });
+    if (!user) {
+      throw new Error(`User with id ${id} not found`);
+    }
+    return user;
+  }
+  updateUser(id: number, updateUserDto: UpdateUserDto) {
+    const user: User = new User();
+    user.name = updateUserDto.name ?? '';
+    user.age = updateUserDto.age ?? 0;
+    user.email = updateUserDto.email ?? '';
+    user.username = updateUserDto.username ?? '';
+    user.password = updateUserDto.password ?? '';
+    user.id = id;
+    return this.userRepository.save(user);
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
+  removeUser(id: number) {
     return `This action removes a #${id} user`;
   }
 }
